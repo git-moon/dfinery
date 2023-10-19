394f6c7acd582f093c9371ef4366c1e5085abd49
MOON <gitgitmoon@gmail.com>
2023-10-19 17:26:04 +0900

    array 요소 순서 변경 util 추가

--- src/util/index.js
+++ src/util/index.js
@@ -2,6 +2,13 @@ export const commaFormat = (value) => {
   return Intl.NumberFormat().format(value)
 }
 
+export const arrayMove = (arr, fromIndex, toIndex) => {
+  const el = arr[fromIndex]
+  arr.splice(fromIndex, 1)
+  arr.splice(toIndex, 0, el)
+}
+
 export default {
   commaFormat,
+  arrayMove,
 }
